47ed7cf141da4f9ee0b1a0668835f0b43b014a1b
jenniferho <jennifersho09@gmail.com>
2020-03-02 12:00:49 -0500

    second commit

--- app/src/main/java/com/example/lifecycle05/MainActivity.java
+++ app/src/main/java/com/example/lifecycle05/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPrefs; //this is for set 2, as long as the app has been installed on phone
     SharedPreferences.Editor editor;
     LCCounter counter;
+
     TextView createTV1;
     TextView startTV1;
     TextView resumeTV1;
@@ -18,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
     TextView stopTV1;
     TextView restartTV1;
     TextView destroyTV1;
+
+    TextView createTV2;
+    TextView startTV2;
+    TextView resumeTV2;
+    TextView pauseTV2;
+    TextView stopTV2;
+    TextView restartTV2;
+    TextView destroyTV2;
  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,17 +40,38 @@ public class MainActivity extends AppCompatActivity {
         stopTV1 = findViewById(R.id.stop);
         restartTV1 = findViewById(R.id.restart);
         destroyTV1 = findViewById(R.id.destroy);
+        ////////////////////////////////////////
+        createTV2 = findViewById(R.id.create2);
+        startTV2 = findViewById(R.id.start2);
+        resumeTV2 = findViewById(R.id.resume2);
+        pauseTV2 = findViewById(R.id.pause2);
+        stopTV2 = findViewById(R.id.stop2);
+        restartTV2 = findViewById(R.id.restart2);
+        destroyTV2 = findViewById(R.id.destroy2);
 
         sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         counter = new LCCounter(0, 0, 0, 0, 0, 0 , 0);
 
-        counter.setCreate(counter.getCreate()+1);
+        counter.setCreate((counter.getCreate())+1);
         editor = sharedPrefs.edit();
-        int count = counter.getCreate();
         int current = sharedPrefs.getInt("create", 0);
-        editor.putInt("start", count+current);
+        Log.i("create current", String.valueOf(current));
+        editor.putInt("create", current+1);
         editor.apply();
-        createTV1.setText("Create -- " + (count+current));
+        createTV2.setText("Create -- " + (current+1));
+        //----------------------------------------------------------
+
+        counter.setDestroy(counter.getDestroy()+1);
+        editor = sharedPrefs.edit();
+        //int destroyCount = counter.getDestroy();
+        int destroyCurrent = sharedPrefs.getInt("destroy", 0);
+        editor.putInt("destroy", 1+current);
+        editor.apply();
+        destroyTV2.setText("Destroy -- " + (1+destroyCurrent));
+
+        pauseTV2.setText("Pause -- " + sharedPrefs.getInt("pause", 0));
+        stopTV2.setText("Stop -- " + sharedPrefs.getInt("stop", 0));
+        restartTV2.setText("Restart -- " + sharedPrefs.getInt("restart", 0));
     }
     @Override
     protected void onStart() {
@@ -50,9 +81,10 @@ public class MainActivity extends AppCompatActivity {
         editor = sharedPrefs.edit();
         int count = counter.getStart();
         int current = sharedPrefs.getInt("start", 0);
-        editor.putInt("start", count+current);
+        editor.putInt("start", current+1);
         editor.apply();
-        startTV1.setText("Start -- " + (count+current));
+        startTV2.setText("Start -- " + (current+1));
+
     }
     @Override
     protected void onResume() {
@@ -61,10 +93,11 @@ public class MainActivity extends AppCompatActivity {
 
         editor = sharedPrefs.edit();
         int count = counter.getResume();
+        Log.i("resume count", String.valueOf(count));
         int current = sharedPrefs.getInt("resume", 0);
-        editor.putInt("resume", count+current);
+        editor.putInt("resume", 1+current);
         editor.apply();
-        resumeTV1.setText("Resume -- " + (count+current));
+        resumeTV2.setText("Resume -- " + (current+1));
     }
     @Override
     protected void onPause() {
@@ -73,10 +106,11 @@ public class MainActivity extends AppCompatActivity {
 
         editor = sharedPrefs.edit();
         int count = counter.getPause();
+        Log.i("pause count", String.valueOf(count));
         int current = sharedPrefs.getInt("pause", 0);
-        editor.putInt("pause", count+current);
+        editor.putInt("pause", 1+current);
         editor.apply();
-        pauseTV1.setText("Pause -- " + (count+current));
+        pauseTV2.setText("Pause -- " + (current+1));
     }
     @Override
     protected void onStop() {
@@ -86,55 +120,51 @@ public class MainActivity extends AppCompatActivity {
         editor = sharedPrefs.edit();
         int count = counter.getStop();
         int current = sharedPrefs.getInt("stop", 0);
-        editor.putInt("stop", count+current);
+        editor.putInt("stop", 1+current);
         editor.apply();
-        stopTV1.setText("Stop -- " + (count+current));
+        stopTV2.setText("Stop -- " + (current+1));
     }
     @Override
     protected void onRestart() {
         super.onRestart();
+
         counter.setRestart(counter.getRestart()+1);
 
         editor = sharedPrefs.edit();
         int count = counter.getRestart();
         int current = sharedPrefs.getInt("restart", 0);
-        editor.putInt("restart", count+current);
+        editor.putInt("restart", 1+current);
         editor.apply();
-        restartTV1.setText("Restart -- " + (count+current));
+        restartTV2.setText("Restart -- " + (current+1));
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        counter.setDestroy(counter.getDestroy()+1);
 
-        editor = sharedPrefs.edit();
-        int count = counter.getDestroy();
-        int current = sharedPrefs.getInt("destroy", 0);
-        editor.putInt("destroy", count+current);
-        editor.apply();
-        destroyTV1.setText("Stop -- " + (count+current));
     }
 
-    public class LCCounter{
-        int create;
-        int start;
-        int resume;
-        int pause;
-        int stop;
-        int restart;
-        int destroy;
 
-        LCCounter(int create, int start, int resume, int pause, int stop, int restart, int destroy) {
-            this.create= create;
-            this.start = start;
-            this.resume = resume;
-            this.pause = pause;
-            this.stop = stop;
-            this.restart = restart;
-            this.destroy = destroy;
-        }
+}
+class LCCounter{
+    private int create;
+    private int start;
+    private int resume;
+    private int pause;
+    private int stop;
+    private int restart;
+    private int destroy;
+
+    LCCounter(int create, int start, int resume, int pause, int stop, int restart, int destroy) {
+        this.create= create;
+        this.start = start;
+        this.resume = resume;
+        this.pause = pause;
+        this.stop = stop;
+        this.restart = restart;
+        this.destroy = destroy;
+    }
     int getCreate(){
-            return create;
+        return create;
     }
     int getStart() {
 
@@ -189,8 +219,8 @@ public class MainActivity extends AppCompatActivity {
     void setDestroy(int destroy) {
         this.destroy = destroy;
     }
+
     void setCreate(int create){
-            this.create = create;
-    }
+        this.create = create;
     }
 }
